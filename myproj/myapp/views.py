3b66a88c32aaa8908eaf97c05ce9c61301d6c245
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-06-03 18:15:05 +0530

    commented

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -41,7 +41,7 @@ def login(request):
                             write.writerow([current_date, current_time])
 
                         return render(request, 'homepage.html')  # Redirect to success page after successful login
-                        # return render(request, 'doctor_homepage.html')
+                        # return render(request, 'doctor_homepage.html') # change to required html file
                     else:
                         return render(request, 'index.html', {'message': 'Wrong password'})  # Display wrong password message
 
