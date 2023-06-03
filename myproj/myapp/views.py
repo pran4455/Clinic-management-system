8c8b6955c598040414d3bb3506565c6f42397afb
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-06-03 18:13:05 +0530

    Done with doctor's homepage and its css

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -40,8 +40,8 @@ def login(request):
 
                             write.writerow([current_date, current_time])
 
-                        # return render(request, 'homepage.html')  # Redirect to success page after successful login
-                        return render(request, 'patient_homepage.html')
+                        return render(request, 'homepage.html')  # Redirect to success page after successful login
+                        # return render(request, 'doctor_homepage.html')
                     else:
                         return render(request, 'index.html', {'message': 'Wrong password'})  # Display wrong password message
 
