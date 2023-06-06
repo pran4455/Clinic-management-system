9309c40d1a030dada760546eef4d820abfcb1365
pran4455 <pranaav4455@gmail.com>
2023-06-06 10:54:17 +0530

    a

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -76,10 +76,12 @@ def newregister(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm-password']
-        address = request.POST['address']
+        address = request.POST['address'].replace('\n',',')
+        address.replace('\r',',')
         age = request.POST['age']
         gender = request.POST['gender']
         blood_group = request.POST['blood-group']
+
 
         if password != confirm_password:
             return render(request, 'register.html', {'alertmessage': 'Passwords do not match.'})
@@ -90,13 +92,13 @@ def newregister(request):
                 if email == row[3]:
                     return render(request, 'register.html', {'alertmessage': 'E-mail already exists.'})
 
-        uniqueid_random = str(random.randint(100000, 999999))
-        while uniqueid_random in [row[-1] for row in reader]:
             uniqueid_random = str(random.randint(100000, 999999))
+            while uniqueid_random in [row[-1] for row in reader]:
+                uniqueid_random = str(random.randint(100000, 999999))
 
         with open('register.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow([name, mobile, dob, email, password, address, age, gender, blood_group, "pat", uniqueid_random)])
+            writer.writerow([name, mobile, dob, email, password, address, age, gender, blood_group, "pat", uniqueid_random])
 
         return render(request, 'index.html', {'alertmessage': 'New user registration information stored successfully.'})
 
